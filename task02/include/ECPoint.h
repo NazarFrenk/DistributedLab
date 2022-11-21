7e8aa8e0c94527a78ee227c3a8b291e5010d6d15
NazarFrenk <nazarfrenk77@gmail.com>
2022-11-21 16:36:18 +0200

    Format

--- task02/include/ECPoint.h
+++ task02/include/ECPoint.h
@@ -13,15 +13,15 @@ class ECPoint
 {
 private:
     long int X, Y;
-    static std::string ECPointToString(ECPoint point); //Convert point to string
+    static std::string ECPointToString(ECPoint point); // Convert point to string
 public:
-    ECPoint(); // G-generator receiving
-    ECPoint(long int x, long int y); // ECPoint creation with pre-defined parameters
-    static bool IsOnCurveCheck(ECPoint a); // P ∈ CURVE?
+    ECPoint();                                        // G-generator receiving
+    ECPoint(long int x, long int y);                  // ECPoint creation with pre-defined parameters
+    static bool IsOnCurveCheck(ECPoint a);            // P ∈ CURVE?
     static ECPoint AddECPoints(ECPoint a, ECPoint b); // P + Q
-    static ECPoint DoubleECPoints(ECPoint a); // 2P
+    static ECPoint DoubleECPoints(ECPoint a);         // 2P
     static ECPoint ScalarMult(ECPoint a, long int k); // k * P
-    static void PrintECPoint(ECPoint point); //Print point
+    static void PrintECPoint(ECPoint point);          // Print point
     ~ECPoint();
 };
 
